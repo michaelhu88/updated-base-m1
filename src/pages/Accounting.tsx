2444c7b1f4c0e3292994fbf5c8caed19cf6e7c78
Michael Hu <michaelhu@Administrators-MacBook-Air-6.local>
2025-11-04 23:02:20 -0600

    Remove test modules

--- src/pages/Accounting.tsx
+++
@@ -1,9 +0,0 @@
-const Accounting = () => {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-semibold">Accounting Module</h1>
-    </div>
-  );
-};
-
-export default Accounting;
--- src/pages/IT.tsx
+++
@@ -1,9 +0,0 @@
-const IT = () => {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-semibold">IT Module</h1>
-    </div>
-  );
-};
-
-export default IT;
